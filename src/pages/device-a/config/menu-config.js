c7bf27781289848ebe79ea2989905f3064d8d8e9
tunca <atuncatunc@gmail.com>
2017-09-28 13:47:57 +0300

    Menu config simplified

--- src/pages/device-a/config/menu-config.js
+++ src/pages/device-a/config/menu-config.js
@@ -4,18 +4,6 @@ const listItems = [
      "children":[  
 
      ],
-     "_styles":{  
-        "root":{  
-           "paddingLeft":-16,
-           "backgroundColor":null,
-           "height":"48px",
-           "cursor":"pointer",
-           "color":"rgba(0,0,0,0.87)",
-           "overflow":"hidden",
-           "transform":"translateZ(0)"
-        }
-     },
-     "_shouldRender":false
   },
   {  
      "title":"Device A Chapter 1: Preamble",
@@ -26,19 +14,6 @@ const listItems = [
         5
      ],
      "disabled":false,
-     "_styles":{  
-        "root":{  
-           "paddingLeft":0,
-           "backgroundColor":null,
-           "height":"48px",
-           "cursor":"pointer",
-           "color":"rgba(0,0,0,0.87)",
-           "overflow":"hidden",
-           "transform":"translateZ(0)"
-        }
-     },
-     "_shouldRender":true,
-     "_primaryText":"Chapter 1: Preamble"
   },
   {  
      "title":"What is Functional Programming",
@@ -49,59 +24,18 @@ const listItems = [
      ],
      "parentIndex":1,
      "disabled":false,
-     "_styles":{  
-        "root":{  
-           "paddingLeft":16,
-           "backgroundColor":null,
-           "height":"48px",
-           "cursor":"pointer",
-           "color":"rgba(0,0,0,0.87)",
-           "overflow":"hidden",
-           "transform":"translateZ(0)"
-        }
-     },
-     "_shouldRender":false,
-     "_primaryText":"What is Functional Programming"
   },
   {  
      "title":"Pure Functions",
      "depth":3,
      "parentIndex":2,
      "disabled":false,
-     "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.",
-     "_styles":{  
-        "root":{  
-           "paddingLeft":32,
-           "backgroundColor":"rgba(0,0,0,0.2)",
-           "height":"48px",
-           "cursor":"pointer",
-           "color":"rgba(0,0,0,0.87)",
-           "overflow":"hidden",
-           "transform":"translateZ(0)"
-        }
-     },
-     "_shouldRender":false,
-     "_primaryText":"Pure Functions"
   },
   {  
      "title":"Composing Functions",
      "depth":3,
      "parentIndex":2,
      "disabled":false,
-     "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.",
-     "_styles":{  
-        "root":{  
-           "paddingLeft":32,
-           "backgroundColor":null,
-           "height":"48px",
-           "cursor":"pointer",
-           "color":"rgba(0,0,0,0.87)",
-           "overflow":"hidden",
-           "transform":"translateZ(0)"
-        }
-     },
-     "_shouldRender":false,
-     "_primaryText":"Composing Functions"
   },
   {  
      "title":"Functional JavaScript",
@@ -112,59 +46,18 @@ const listItems = [
      ],
      "disabled":false,
      "parentIndex":1,
-     "_styles":{  
-        "root":{  
-           "paddingLeft":16,
-           "backgroundColor":null,
-           "height":"48px",
-           "cursor":"pointer",
-           "color":"rgba(0,0,0,0.87)",
-           "overflow":"hidden",
-           "transform":"translateZ(0)"
-        }
-     },
-     "_shouldRender":false,
-     "_primaryText":"Functional JavaScript"
   },
   {  
      "title":"JavaScript Array Methods",
      "depth":3,
      "parentIndex":5,
      "disabled":false,
-     "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.",
-     "_styles":{  
-        "root":{  
-           "paddingLeft":32,
-           "backgroundColor":null,
-           "height":"48px",
-           "cursor":"pointer",
-           "color":"rgba(0,0,0,0.87)",
-           "overflow":"hidden",
-           "transform":"translateZ(0)"
-        }
-     },
-     "_shouldRender":false,
-     "_primaryText":"JavaScript Array Methods"
   },
   {  
      "title":"Introduction to Ramda",
      "depth":3,
      "parentIndex":5,
      "disabled":false,
-     "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.",
-     "_styles":{  
-        "root":{  
-           "paddingLeft":32,
-           "backgroundColor":null,
-           "height":"48px",
-           "cursor":"pointer",
-           "color":"rgba(0,0,0,0.87)",
-           "overflow":"hidden",
-           "transform":"translateZ(0)"
-        }
-     },
-     "_shouldRender":false,
-     "_primaryText":"Introduction to Ramda"
   },
   {  
      "title":"Chapter 2: React",
@@ -175,19 +68,6 @@ const listItems = [
         9,
         12
      ],
-     "_styles":{  
-        "root":{  
-           "paddingLeft":0,
-           "backgroundColor":null,
-           "height":"48px",
-           "cursor":"pointer",
-           "color":"rgba(0,0,0,0.87)",
-           "overflow":"hidden",
-           "transform":"translateZ(0)"
-        }
-     },
-     "_shouldRender":true,
-     "_primaryText":"Chapter 2: React"
   },
   {  
      "title":"Introduction to React",
@@ -198,122 +78,40 @@ const listItems = [
         10,
         11
      ],
-     "_styles":{  
-        "root":{  
-           "paddingLeft":16,
-           "backgroundColor":null,
-           "height":"48px",
-           "cursor":"pointer",
-           "color":"rgba(0,0,0,0.87)",
-           "overflow":"hidden",
-           "transform":"translateZ(0)"
-        }
-     },
-     "_shouldRender":false,
-     "_primaryText":"Introduction to React"
   },
   {  
      "title":"Writing React Components",
      "depth":3,
      "parentIndex":9,
      "disabled":false,
-     "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.",
-     "_styles":{  
-        "root":{  
-           "paddingLeft":32,
-           "backgroundColor":null,
-           "height":"48px",
-           "cursor":"pointer",
-           "color":"rgba(0,0,0,0.87)",
-           "overflow":"hidden",
-           "transform":"translateZ(0)"
-        }
-     },
-     "_shouldRender":false,
-     "_primaryText":"Writing React Components"
   },
   {  
      "title":"Composing React Components",
      "depth":3,
      "parentIndex":9,
      "disabled":false,
-     "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.",
-     "_styles":{  
-        "root":{  
-           "paddingLeft":32,
-           "backgroundColor":null,
-           "height":"48px",
-           "cursor":"pointer",
-           "color":"rgba(0,0,0,0.87)",
-           "overflow":"hidden",
-           "transform":"translateZ(0)"
-        }
-     },
-     "_shouldRender":false,
-     "_primaryText":"Composing React Components"
   },
   {  
      "title":"React Components",
      "depth":2,
      "parentIndex":8,
-     "disabled":true,
+     "disabled":false,
      "children":[  
         13,
         14
      ],
-     "_styles":{  
-        "root":{  
-           "paddingLeft":16,
-           "backgroundColor":null,
-           "height":"48px",
-           "cursor":"not-allowed",
-           "color":"rgba(0,0,0,0.4)",
-           "overflow":"hidden",
-           "transform":"translateZ(0)"
-        }
-     },
-     "_shouldRender":false,
-     "_primaryText":"React Components"
   },
   {  
      "title":"Props and State in React",
      "parentIndex":12,
      "depth":3,
      "disabled":true,
-     "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.",
-     "_styles":{  
-        "root":{  
-           "paddingLeft":32,
-           "backgroundColor":null,
-           "height":"48px",
-           "cursor":"not-allowed",
-           "color":"rgba(0,0,0,0.4)",
-           "overflow":"hidden",
-           "transform":"translateZ(0)"
-        }
-     },
-     "_shouldRender":false,
-     "_primaryText":"Props and State in React"
   },
   {  
      "title":"Component Lifecycle",
      "parentIndex":12,
      "depth":3,
      "disabled":true,
-     "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.",
-     "_styles":{  
-        "root":{  
-           "paddingLeft":32,
-           "backgroundColor":null,
-           "height":"48px",
-           "cursor":"not-allowed",
-           "color":"rgba(0,0,0,0.4)",
-           "overflow":"hidden",
-           "transform":"translateZ(0)"
-        }
-     },
-     "_shouldRender":false,
-     "_primaryText":"Component Lifecycle"
   }
 ]
 
