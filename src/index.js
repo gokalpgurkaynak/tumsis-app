d34b0ce9ece6e9fbc2399b7c027374f3470175d0
Gökalp Gürkaynak <tr1g5087@icterra.net>
2017-10-19 09:20:26 +0300

    Grommet added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { render } from 'react-dom';
 import Index from './pages/index';
 import 'typeface-roboto'
 import 'url-search-params-polyfill';
+import 'grommet/grommet.min.css'
 
 
 render(<Index />, document.querySelector('#root'));
--- src/pages/grommet/index.js
+++ src/pages/grommet/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Map from 'grommet/components/Map';
+
 import { 
     Grid,
     Paper,
     Typography
-} from 'material-ui'
+} from 'material-ui';
 import { withStyles } from 'material-ui/styles';
   
 const styles = theme => ({
@@ -13,13 +15,19 @@ const styles = theme => ({
   },
   paper: {
     padding: 16,
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    'align-items': 'center'
+    color: theme.palette.text.secondary
   },
   button: {
     margin: theme.spacing.unit * 3,
     textTransform: 'none'
+  },
+  typography: {
+    textAlign: 'center',
+    'align-items': 'center'
+  },
+  boxClass: {
+    textAlign: 'center',
+    'align-items': 'center'
   }
 });
 
@@ -28,13 +36,45 @@ class Grommet extends Component {
     render = () => {
         const classes = this.props.classes;
         return (
-            <Grid container spacing={24}>
+            <Grid container spacing={12}>
                 <Grid item xs={12}>
                 <Paper className={classes.paper}>
-                <Typography type="headline" component="h1">
+                <Typography type="headline" component="h1" className={classes.typography}>
                     Grommet
                 </Typography>
-                    
+                <Map data={{
+                        "categories": [
+                            {
+                            "id": "category-1",
+                            "items": [
+                                {"id": "Modem", "label": "Modem"},
+                                {"id": "SSPA", "label": "SSPA"},
+                                {"id": "Beacon-1", "label": "Beacon 1"}
+                            ]
+                            },
+                            {
+                            "id": "category-2",
+                            "items": [
+                                {"id": "Beacon-2", "label": "Beacon 2"},
+                                {"id": "ACU", "label": "ACU"}
+                            ]
+                            },
+                            {
+                            "id": "category-3",
+                            "items": [
+                                {"id": "MTek-Down-Converter", "label": "MTek Down Converter"},
+                                {"id": "MTek-Up-Converter", "label": "MTek Up Converter"}
+                            ]
+                            }
+                        ],
+                        "links": [
+                            {"parentId": "Modem", "childId": "SSPA"},
+                            {"parentId": "SSPA", "childId": "Beacon-2"},
+                            {"parentId": "Beacon-1", "childId": "MTek-Down-Converter"},
+                            {"parentId": "ACU", "childId": "SSPA"},
+                            {"parentId": "MTek-Up-Converter", "childId": "SSPA"}
+                        ]
+                        }} />
                 </Paper>
                 </Grid>
             </Grid>
